80cecf8c999dbb0f5caeb7aea0149f4e77b73e60
snehaldigitalsalt <snehal@digitalsalt.in>
2021-05-01 18:52:19 +0530

    changes' ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,43 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
+import Img from "gatsby-image"
+const IndexPage = ({data}) => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <h1>Hi people</h1>
+  <p>Welcome to your new Gatsby site.</p>
+  <p>Now go build something great.</p>
+
+  {data.allStrapiArticles.nodes.map((item, index) => (
+    <li key={index}>
+      <h2>{item.title}</h2>
+      <Img fixed={item.image.childImageSharp.fixed} />
+      <p>{item.content}</p>
+    </li>
+  ))}
+</Layout>
+
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query MyQuery {
+    allStrapiArticles {
+      nodes {
+        id
+        title
+        content
+        image {
+          id
+          childImageSharp {
+            fixed(width: 400, height: 200
+            ) {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
+      }
+    }
+  }
+`
